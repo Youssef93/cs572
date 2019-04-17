f0cdb46799dad5be689aa22d8a2d97a59b55b7b5
Youssef Fayez Fouad Louka <ylouka@mum.edu>
2019-04-17 16:43:55 -0500

    solved hw 13

---
+++ homework_13/download-users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DownloadUsersService {
+
+  constructor(public http: HttpClient) {
+    this.downloadToCache();
+  }
+
+  async getOnlineData() {
+    return await this.downloadToCache();
+  }
+
+  downloadToCache() {
+    return new Promise((resolve, reject) => {
+      this.http.get('https://randomuser.me/api/?results=10')
+        .subscribe(result => {
+          localStorage.setItem('users', JSON.stringify(result));
+          resolve(result);
+        });
+    })
+  }
+
+  getCachedData() {
+    return JSON.parse(localStorage.getItem('users'));
+  }
+}
